157c646cba96f801283843b052dcf8b4b037c00e
nuray-alam <siddik4x@gmail.com>
2021-11-07 12:10:39 +0600

    get orders api added

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ async function run() {
         })
 
         //Add orders API
+
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.json(orders);
+        })
         app.post('/orders', async (req, res) => {
 
             const order = req.body;
